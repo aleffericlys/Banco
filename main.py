4197e0b415395be7bf31c0d761935e316b27d64f
aleffericlys <ericlys558@gamil.com>
2021-09-28 10:19:09 -0300

    adicionando main

---
+++ main.py
@@ -0,0 +1,12 @@
+from conta import Conta, Cliente
+
+p1 = Cliente('aleff', 'erl', '123455')
+p2 = Cliente('Ffela', 'lre', '554321')
+c = Conta('1234', p1, 500, 1000 )
+b = Conta('4321', p2, 500, 1000 )
+c.sacar(100)
+b.transferir(c, 200)
+# print(c.saldo)
+# print(b.saldo)
+c.extratos()
+b.extratos()
